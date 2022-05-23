abff24f3e349dcf320c7cdfc80f531573f7336af
Alexander Batres <alex2000.sabe@gmail.com>
2022-05-23 15:53:31 -0600

    Player entity created

---
+++ PlayerEntity.cpp
@@ -0,0 +1,29 @@
+#include "PlayerEntity.h"
+
+PlayerEntity::PlayerEntity(
+	Int32^ id,
+	Int32^ selection_id,
+	String^ name,
+	String^ number_shirt,
+	Position position,
+	Int32^ age,
+	String^ skills,
+	Leg leg,
+	Boolean^ is_nationalized,
+	String^ belonging_team,
+	String^ sponsor,
+	Single^ market_value)
+{
+	this->id = id;
+	this->selection_id = selection_id;
+	this->name = name;
+	this->number_shirt = number_shirt;
+	this->position = position;
+	this->age = age;
+	this->skills = skills;
+	this->leg = leg;
+	this->is_nationalized = is_nationalized;
+	this->belonging_team = belonging_team;
+	this->sponsor = sponsor;
+	this->market_value = market_value;
+}
---
+++ PlayerEntity.h
@@ -0,0 +1,34 @@
+#pragma once
+
+using namespace System;
+
+enum Position
+{
+	Central, Medio, Offensive
+};
+
+enum Leg
+{
+	Right, Left,
+};
+
+ref class PlayerEntity
+{
+public:
+	PlayerEntity(Int32^, Int32^, String^, String^, Position,
+		Int32^, String^, Leg, Boolean^, String^,
+		String^, Single^);
+
+	Int32^ id;
+	Int32^ selection_id;
+	String^ name;
+	String^ number_shirt;
+	Position position;
+	Int32^ age;
+	String^ skills;
+	Leg leg;
+	Boolean^ is_nationalized;
+	String^ belonging_team;
+	String^ sponsor;
+	Single^ market_value;
+};
