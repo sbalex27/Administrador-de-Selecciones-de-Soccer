6db277c3c76bc6c3656e31efa39626f14ad15ffa
Alexander Batres <alex2000.sabe@gmail.com>
2022-05-23 15:57:24 -0600

    Area Entity created

---
+++ AreaEntity.cpp
@@ -0,0 +1,7 @@
+#include "AreaEntity.h"
+
+AreaEntity::AreaEntity(Int32^ id, String^ name)
+{
+	this->id = id;
+	this->name = name;
+}
---
+++ AreaEntity.h
@@ -0,0 +1,11 @@
+using namespace System;
+
+#pragma once
+ref class AreaEntity
+{
+	AreaEntity(Int32^, String^);
+
+	Int32^ id;
+	String^ name;
+};
+
