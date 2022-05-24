0e5a3ef25dc1f1e66a9a6ec0536fe87bb4f6d063
Alexander Batres <alex2000.sabe@gmail.com>
2022-05-23 18:15:10 -0600

    Abstract struct selection repository

---
+++ SelectionRepository.h
@@ -0,0 +1,37 @@
+#pragma once
+#include<vector>
+#include "SelectionEntity.h";
+
+ref struct SelectionRepository abstract {
+	/// <summary>
+	/// Fetch and index all the results
+	/// </summary>
+	/// <returns>A collection of selection entities</returns>
+	virtual std::vector<SelectionEntity^> index() abstract;
+
+	/// <summary>
+	/// Show an specified selection
+	/// </summary>
+	/// <param name="id">Selection find property</param>
+	/// <returns>A instance of selection entity</returns>
+	virtual SelectionEntity^ show(Int32^ id) abstract;
+
+	/// <summary>
+	/// Store a new resource in the storage
+	/// </summary>
+	/// <param name="selection">Entity to storage</param>
+	virtual void store(SelectionEntity^ selection) abstract;
+
+	/// <summary>
+	/// Update the specified resource in the repository
+	/// </summary>
+	/// <param name="id">Selection find property</param>
+	/// <param name="selection">A instance of selection entity</param>
+	virtual void update(Int32^ id, SelectionEntity^ selection) abstract;
+
+	/// <summary>
+	/// Destroy the specified resource in the repository
+	/// </summary>
+	/// <param name="id">Selection find property</param>
+	virtual void destroy(Int32^ id) abstract;
+};
